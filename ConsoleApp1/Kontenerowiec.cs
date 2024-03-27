122f9a1721e98f5a601ed919bf5d0d9e36455493
Mikołaj Kowalski <s27630@pjwstk.edu.pl>
2024-03-27 12:07:36 +0100

    done

---
+++ ConsoleApp1/Kontenerowiec.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+
+    public class Kontenerowiec
+    {
+        public List<Kontener> Containers { get; private set; }
+        public int MaxSpeed { get; private set; }
+        public int MaxContainers { get; private set; }
+        public double MaxTotalWeight { get; private set; }
+
+        public Kontenerowiec(int maxSpeed, int maxContainers, double maxTotalWeight)
+        {
+            Containers = new List<Kontener>();
+            MaxSpeed = maxSpeed;
+            MaxContainers = maxContainers;
+            MaxTotalWeight = maxTotalWeight;
+        }
+
+        public void AddContainer(Kontener container)
+        {
+            if (Containers.Count < MaxContainers)
+            {
+                Containers.Add(container);
+            }
+            else
+            {
+                Console.WriteLine("Maksymalna liczba kontenerów.");
+            }
+        }
+
+        public double CalculateTotalWeight()
+        {
+            double totalWeight = 0;
+            foreach (Kontener container in Containers)
+            {
+                totalWeight += container.GetLoadWeight();
+            }
+            return totalWeight;
+        }
+
+    
+        public bool IsReadyForVoyage()
+        {
+            if (CalculateTotalWeight() <= MaxTotalWeight)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public void DropContener()
+        {
+            Console.WriteLine("Podaj numer seryjny kontenera do usunięcia (w formacie KON-X-Numer):");
+            string id = Console.ReadLine(); 
+
+            foreach (var container in Containers.ToList())  
+            {
+                if (id.Equals(container.SerialNumber))
+                {
+                    Containers.Remove(container); 
+                    Console.WriteLine($"Kontener {id} został usunięty ze statku.");
+                    return; 
+                }
+            }
+
+            Console.WriteLine("Nie znaleziono kontenera o podanym numerze seryjnym na statku.");
+        }
+        public void ReplaceContainer(Kontener newContainer)
+        {
+            Console.WriteLine("Podaj numer seryjny kontenera, który chcesz zastąpić (w formacie KON-X-Numer):");
+            string oldContainerId = Console.ReadLine();
+
+            foreach (var container in Containers.ToList()) 
+            {
+                if (oldContainerId.Equals(container.SerialNumber))
+                {
+                    Containers.Remove(container); 
+                    Containers.Add(newContainer); 
+                    Console.WriteLine($"Kontener {oldContainerId} został zastąpiony nowym kontenerem.");
+                    return; 
+                }
+            }
+
+            Console.WriteLine("Nie znaleziono kontenera o podanym numerze seryjnym na statku.");
+        }
+
+        public void MoveContainer(Kontener container, Kontenerowiec targetShip)
+        {
+            if (Containers.Contains(container)) 
+            {
+                if (targetShip.Containers.Count < targetShip.MaxContainers) 
+                {
+                    Containers.Remove(container); 
+                    targetShip.Containers.Add(container); 
+                    Console.WriteLine($"Kontener {container.SerialNumber} został przeniesiony na inny statek.");
+                }
+                else
+                {
+                    Console.WriteLine("Docelowy statek ma już maksymalną liczbę kontenerów. Nie można przenieść kontenera.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Podany kontener nie znajduje się na bieżącym statku. Nie można go przenieść.");
+            }
+        }
+
+
+        public void PrintShipInfo()
+        {
+            Console.WriteLine($"Informacje o statku:");
+            Console.WriteLine($"Maksymalna prędkość: {MaxSpeed} węzłów");
+            Console.WriteLine($"Maksymalna liczba kontenerów: {MaxContainers}");
+            Console.WriteLine($"Maksymalna waga ładunku: {MaxTotalWeight} ton\n");
+
+            Console.WriteLine($"Ładunek na statku:");
+            if (Containers.Count > 0)
+            {
+                foreach (Kontener container in Containers)
+                {
+                    Console.WriteLine($"Numer seryjny: {container.SerialNumber}");
+                    Console.WriteLine($"Masa ładunku: {container.GetLoadWeight()} kg");
+                    Console.WriteLine($"Wysokość: {container.Height} cm");
+                    Console.WriteLine($"Waga własna: {container.OwnWeight} kg");
+                    Console.WriteLine($"Głębokość: {container.Depth} cm");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Brak kontenerów na statku.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Przykładowe kontenery i ich opis
+        Plyny plyny1 = new Plyny(1000, 200, 50, 150, 2000, 500, false);
+        Plyny plyny2 = new Plyny(1500, 180, 60, 140, 2500, 700, true);
+        Plyny plyny3 = new Plyny(1200, 190, 55, 160, 2200, 600, false);
+
+        //Do zamiany
+        Plyny plyny4 = new Plyny(1100, 210, 55, 170, 2300, 650, false);
+
+
+        plyny1.PrintContainerInfo();
+        plyny2.PrintContainerInfo();
+        plyny3.PrintContainerInfo();
+
+        //Stworzenie kontenera oraz dodanie załadowanie go 
+        Kontenerowiec kontenerowiec = new Kontenerowiec(100, 100, 100000);
+
+        Kontenerowiec kontenerowiec1 = new Kontenerowiec(100, 100, 10000);
+
+        kontenerowiec.AddContainer(plyny1);
+        kontenerowiec.AddContainer(plyny2);
+        kontenerowiec.AddContainer(plyny3);
+
+
+    
+        //info
+        kontenerowiec.PrintShipInfo();
+
+        //usuniecie kontenera
+        kontenerowiec.DropContener();
+
+        //info po usnięciu 
+        kontenerowiec.PrintShipInfo();
+
+        //zastąpienie kontenera innym
+        kontenerowiec.ReplaceContainer(plyny4);
+
+        //into po zastąpieniu
+        kontenerowiec.PrintShipInfo();
+
+        //przesuniecie konterenra
+        kontenerowiec.MoveContainer(plyny1, kontenerowiec1);
+
+        kontenerowiec1.PrintShipInfo();
+
+
+    }
+}
